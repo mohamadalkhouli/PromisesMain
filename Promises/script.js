37ef7e8e16e15799f63d4e6b21b2f931b883bda3
Mohamad Alkhouli <m_b_alkhouli@.com>
2024-08-14 14:17:55 +0300

    "async and await"

--- Promises/script.js
+++ Promises/script.js
@@ -1,41 +1,59 @@
-setTimeout(function () {
-    document.getElementById("title1").style.visibility = "visible";
+// setTimeout(function () {
+//     document.getElementById("title1").style.visibility = "visible";
 
-    setTimeout(function () {
-        document.getElementById("title2").style.visibility = "visible";
+//     setTimeout(function () {
+//         document.getElementById("title2").style.visibility = "visible";
 
-        setTimeout(function () {
-            document.getElementById("title3").style.visibility = "visible";
-            setTimeout(function () {
-                document.getElementById("title4").style.visibility = "visible";
-            }, 1000)
+//         setTimeout(function () {
+//             document.getElementById("title3").style.visibility = "visible";
+//             setTimeout(function () {
+//                 document.getElementById("title4").style.visibility = "visible";
+//             }, 1000)
+//         }, 1000)
+//     }, 1000)
+// }, 1000)
+
+async function showTitles() {
+    await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.getElementById("title1").style.visibility = "visible";
+            resolve()
         }, 1000)
-    }, 1000)
-}, 1000)
+    })
+    await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.getElementById("title2").style.visibility = "visible";
+            resolve()
+        }, 1000)
+    })
+    await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.getElementById("title3").style.visibility = "visible";
+            resolve()
+        }, 1000)
+    })
 
-let p = new Promise(function (resolve, reject) {
-    console.log("starting the async code")
-    setTimeout(function () {
-        document.getElementById("title1").style.visibility = "visible";
-    }, 2000)
-    isSuccess = true // Change this value to simulate success or failure
-    if (isSuccess) {
-        console.log("calling resolve")
+    setTimeout(() => {
+        document.getElementById("title4").style.visibility = "visible";
         resolve()
-    } else {
-        console.log("calling reject")
-        reject()
-    }
-})
+    }, 1000)
+}
 
-console.log("after the promise creation then and catch")
+showTitles()
+// .then(() => {
 
-p.then(function () {
-    console.log("calling then")
-}).catch(function () {
-    console.log("Failed")
-})
-
-console.log("after the then and catch")
-
-
+// }).then(() => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             document.getElementById("title3").style.visibility = "visible";
+//             resolve()
+//         }, 1000)
+//     })
+// }).then(() => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             document.getElementById("title4").style.visibility = "visible";
+//             resolve()
+//         }, 1000)
+//     })
+// }) 
