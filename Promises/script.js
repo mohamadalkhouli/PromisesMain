0889b8d25a951e6441cbb420e1c519c7aced2e45
Mohamad Alkhouli <m_b_alkhouli@.com>
2024-08-14 14:22:48 +0300

    done

--- Promises/script.js
+++ Promises/script.js
@@ -1,17 +1,3 @@
-// setTimeout(function () {
-//     document.getElementById("title1").style.visibility = "visible";
-
-//     setTimeout(function () {
-//         document.getElementById("title2").style.visibility = "visible";
-
-//         setTimeout(function () {
-//             document.getElementById("title3").style.visibility = "visible";
-//             setTimeout(function () {
-//                 document.getElementById("title4").style.visibility = "visible";
-//             }, 1000)
-//         }, 1000)
-//     }, 1000)
-// }, 1000)
 
 async function showTitles() {
     await new Promise((resolve, reject) => {
@@ -32,28 +18,9 @@ async function showTitles() {
             resolve()
         }, 1000)
     })
-
     setTimeout(() => {
         document.getElementById("title4").style.visibility = "visible";
         resolve()
     }, 1000)
 }
-
 showTitles()
-// .then(() => {
-
-// }).then(() => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             document.getElementById("title3").style.visibility = "visible";
-//             resolve()
-//         }, 1000)
-//     })
-// }).then(() => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             document.getElementById("title4").style.visibility = "visible";
-//             resolve()
-//         }, 1000)
-//     })
-// }) 
